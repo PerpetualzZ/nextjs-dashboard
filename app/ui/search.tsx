c7f2a01661408c1c3e64ce0767a5c42b610f72e6
shixiong <shixiong327926@gmail.com>
2024-03-11 23:21:30 +0800

    Update search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,7 +5,7 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 
 // 防抖
-import { useDebounce, useDebouncedCallback } from 'use-debounce';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   // 使用useSearchParams钩子获取当前URL的查询参数
