80056ac8544124181b776183821babc95300c743
shixiong <shixiong327926@gmail.com>
2024-03-12 17:30:57 +0800

    Mutating Data

---
+++ app/lib/actions.ts
@@ -0,0 +1,36 @@
+'use server'; // 文件中的所有导出函数标记为服务器函数
+
+import { z } from 'zod'; //类型验证库
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache'; // 除此缓存并向服务器触发新请求
+import { redirect } from 'next/navigation'; // 重定向
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amoutInCents = amount * 100;
+  // 获取当前日期 转换为 ISO 8601 格式的字符串
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${status}, ${date})
+  `;
+
+  // 数据库更新后，/dashboard/invoices 路径将重新验证，并且将从服务器获取新数据。
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
