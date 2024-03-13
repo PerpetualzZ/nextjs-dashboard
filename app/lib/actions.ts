2646b04d3169c827bd694c0e852588366a7822a3
shixiong <shixiong327926@gmail.com>
2024-03-13 23:44:55 +0800

    Improving Accessibility

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -83,17 +83,17 @@ export async function updateInvoice(
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-
+ 
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       message: 'Missing Fields. Failed to Update Invoice.',
     };
   }
-
+ 
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
-
+ 
   try {
     await sql`
       UPDATE invoices
@@ -103,7 +103,7 @@ export async function updateInvoice(
   } catch (error) {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
-
+ 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
